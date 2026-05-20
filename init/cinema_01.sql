6468e173b08b9915e73026e25f73df2bf35a31f5
medina5 <emmanuel.medina@univ-lorraine.fr>
2026-05-20 23:30:25 +0200

    correction cinema

--- init/cinema_01.sql
+++ init/cinema_01.sql
@@ -14,9 +14,9 @@ alter database cinema set search_path = public,extensions;
 
 create schema extensions authorization pg_database_owner;
 
-create extension postgis schema extensions;
+create extension postgis;
 
-create extension if not exists fuzzystrmatch schema extensions;
+create extension if not exists fuzzystrmatch;
 create extension if not exists unaccent;
 
 set role cinema;
@@ -825,13 +825,13 @@ end
 $body$;
 
 create function films_par_acteur (id varchar)
-RETURNS TABLE(film_id uuid, titre text, titre_original text,
+RETURNS TABLE(film_id int, titre text, titre_original text,
 annee int, sortie date, duree int, votants int, vote_moyenne numeric(4,2), franchise text, alias text[], genres text[], motscles text[], resume text) AS
 $$
 BEGIN
   return query select f.film_id, f.titre, f.titre_original,
-      f.annee, f.sortie, f.duree, f.vote_votants, f.vote_moyenne,
-      f2.franchise
+      f.annee, f.sortie, f.duree, v.votants, v.moyenne,
+      f2.serie
       , array_agg(distinct e.alias) as alias
       , array_agg(distinct g.genre) as genres
       , array_agg(distinct mc.motcle) as motscles
@@ -840,15 +840,16 @@ BEGIN
     inner join equipes e on e.film_id = f.film_id and e.alias is not null
     left join films_genres fg on fg.film_id = f.film_id
     left join genres g on g.genre_id = fg.genre_id
-    left join franchises f2 on f2.franchise_id = f.franchise_id
+    left join series f2 on f2.serie_id = f.serie_id
+    left join votes v on v.film_id = f.film_id
     left join lateral (SELECT r2.resume FROM resumes r2 WHERE r2.film_id  = f.film_id
-      order by array_position(array['deu','fra','eng'], r2.langue_code)
+      order by array_position(array['deu','fra','eng'], r2.langue)
       fetch first 1 row only ) r on true
     left join films_motscles fmc on fmc.film_id = f.film_id
     left join motscles mc on mc.motcle_id = fmc.motcle_id
-    where e.personne_id = id::uuid
-    group by f.film_id, f.titre, f.titre_original, f2.franchise, f.vote_votants, e.ordre, r.resume
-    order by e.ordre, vote_votants desc;
+    where e.personne_id = id::int
+    group by f.film_id, f.titre, f.titre_original, f2.serie, v.votants, v.moyenne, e.ordre, r.resume
+    order by e.ordre, v.votants desc;
 END;
 $$
 language plpgsql volatile;

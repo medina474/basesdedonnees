ef53ed2527b75ff09b6701bdc3b773a932285fd4
medina5 <emmanuel.medina@univ-lorraine.fr>
2026-06-08 20:38:18 +0200

    aviation

--- init/00.sql
+++ init/00.sql
@@ -48,5 +48,6 @@ create role transport noinherit login password '9512';
 create role gravity noinherit login password '9117';
 create role chinook noinherit login password '9371';
 create role geo noinherit login password '9240';
+create role aviation noinherit login password '9917';
 
 select * from pg_user;
